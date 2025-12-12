0c03407304c0b8911328f8071f380031808a3b7b
devMrkuzz <send2hire.mark@gmail.com>
2025-12-12 10:54:06 +0800

    fixed appwrite.ts

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,9 +2,10 @@ import { Client, Account } from "appwrite";
 
 const client = new Client();
 
+// Use environment variables for flexibility between localhost and production
 client
-  .setEndpoint("https://fra.cloud.appwrite.io/v1") // your endpoint
-  .setProject("693b5308001cb1d4f1f7"); // your project ID
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "https://fra.cloud.appwrite.io/v1")
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT || "693b5308001cb1d4f1f7");
 
 export const account = new Account(client);
 export default client;
